26988f0dc50cc6e3c1bee96812a2d571d8754c06
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-06-20 11:19:35 -0400

    Working wrapper for excel.

--- MatherCPPtoVBA/mather.cpp
+++ MatherCPPtoVBA/mather.cpp
@@ -2,12 +2,22 @@
 #include "pch.h"
 #include "Mather.h"
 #include "MatherCPP.h"
+#include  <stdlib.h>
 
-void* WINAPI mather_create_1()
+struct mather {
+	void* obj;
+};
+
+mather_t* WINAPI mather_create_1()
 {
-	void* m;
-	MatherCPP obj;
-	m = &obj;
+	mather_t* m;
+	MatherCPP* obj;
+
+	m = (mather_t*)malloc(sizeof(m));
+	obj = new MatherCPP();
+	m->obj = obj;
+
+	obj->delta_num1(10.0);
 
 	return m;
 }
@@ -32,11 +42,12 @@ void* WINAPI mather_create_3(double start1, double start2)
 
 //deprecated. You should not need to use, as object should only be destroyed when Excel
 //shuts down anyway, thus having the system automatically handle memory cleanup.
-void WINAPI mather_destroy(void* m)
+void WINAPI mather_destroy(mather_t* m)
 {
 	if (m == NULL)
 		return;
-	delete (MatherCPP*)m;
+	delete static_cast<MatherCPP*>(m->obj);
+	free(m);
 }
 
 double WINAPI mather_add_1(void* m)
@@ -55,11 +66,11 @@ double WINAPI mather_add_2(void* m, double x)
 	return obj->add_num_one(x);
 }
 
-void WINAPI mather_delta_1(void* m, double x)
+void WINAPI mather_delta_1(mather_t* m, double x)
 {
 	if (m == NULL)
 		return;
-	MatherCPP* obj = (MatherCPP*)m;
+	MatherCPP* obj = static_cast<MatherCPP*>(m->obj);
 	return obj->delta_num1(x);
 }
 
@@ -71,21 +82,21 @@ void WINAPI mather_delta_2(void* m, double x)
 	return obj->delta_num2(x);
 }
 
-double WINAPI mather_sum(void* m, double x, double y)
+double WINAPI mather_sum(mather_t* m, double x, double y)
 {
 	if (m == NULL)
 		return -1.0;
-	MatherCPP* obj = (MatherCPP*)m;
+	MatherCPP* obj = static_cast<MatherCPP*>(m->obj);
 	return obj->sum(x, y);
 }
 
 
 //C style wrapped getters for the MatherCPP class
-double WINAPI mather_get_one(void* m)
+double WINAPI mather_get_one(mather_t* m)
 {
 	if (m == NULL)
 		return -1.0;
-	MatherCPP* obj = (MatherCPP*)m;
+	MatherCPP* obj = static_cast<MatherCPP*>(m->obj);
 	return obj->get_num1();
 }
 
--- MatherCPPtoVBA/mather.h
+++ MatherCPPtoVBA/mather.h
@@ -1,6 +1,6 @@
 //C wrapper header file for the MatherCPP class
 #pragma once
-#ifdef MATHERLIB_EXPORTS
+#ifdef MATHERCPPTOVBA_EXPORTS
 #define MATHER_API __declspec(dllexport)
 #else
 #define MATHER_API __declspec(dllimport)
@@ -8,17 +8,19 @@
 
 extern "C"
 {
+	struct mather;
+	typedef struct mather mather_t;
 
-	MATHER_API void* mather_create_1();
+	MATHER_API mather_t* mather_create_1();
 	MATHER_API void* mather_create_2(double start);
 	MATHER_API void* mather_create_3(double start1, double start2);
-	MATHER_API void mather_destroy(void* m);
+	MATHER_API void mather_destroy(mather_t* m);
 	MATHER_API double mather_add_1(void* m);
 	MATHER_API double mather_add_2(void* m, double val);
-	MATHER_API void mather_delta_1(void* m, double val);
+	MATHER_API void mather_delta_1(mather_t* m, double val);
 	MATHER_API void mather_delta_2(void* m, double val);
-	MATHER_API double mather_sum(void* m, double x, double y);
-	MATHER_API double mather_get_one(void* m);
+	MATHER_API double mather_sum(mather_t* m, double x, double y);
+	MATHER_API double mather_get_one(mather_t* m);
 	MATHER_API double mather_get_two(void* m);
 
 }
